06967a0aa12d37ebdf0a60cd867bf8db048898c9
Mostafa Hafezypoor <m.folad1383@gmail.com>
2026-09-07 16:15:28 +0330

    write simple ui steper add order view_post_channel

--- app/src/main/java/mostafa/hafezypoor/robiyar/data/remote/RetrofitInit.kt
+++ app/src/main/java/mostafa/hafezypoor/robiyar/data/remote/RetrofitInit.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInit {
-    private const val BASE_URL= "http://192.168.1.2/robiyar/"
+    private const val BASE_URL= "http://192.168.1.5/robiyar/"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
--- app/src/main/java/mostafa/hafezypoor/robiyar/ui/messenger/addOrder/FViewPostChannel.kt
+++ app/src/main/java/mostafa/hafezypoor/robiyar/ui/messenger/addOrder/FViewPostChannel.kt
@@ -1,6 +1,24 @@
 package mostafa.hafezypoor.robiyar.ui.messenger.addOrder
 
+import android.os.Bundle
+import android.view.View
+import android.widget.ViewFlipper
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
+import mostafa.hafezypoor.robiyar.R
+import kotlin.random.Random
 
-class FViewPostChannel : Fragment(){
+class FViewPostChannel : Fragment(R.layout.fview_post_channel){
+    private lateinit var viewFlipper: ViewFlipper
+    private lateinit var btnV1_Understand : MaterialButton
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewFlipper = view.findViewById<ViewFlipper>(R.id.viewFlipper)
+        btnV1_Understand = view.findViewById<MaterialButton>(R.id.btnV1_Understand)
+        viewFlipper.setInAnimation(context, android.R.anim.fade_in)
+        viewFlipper.setInAnimation(context,android.R.anim.fade_out)
+        btnV1_Understand.setOnClickListener {
+            viewFlipper.showNext()
+        }
+    }
 }
