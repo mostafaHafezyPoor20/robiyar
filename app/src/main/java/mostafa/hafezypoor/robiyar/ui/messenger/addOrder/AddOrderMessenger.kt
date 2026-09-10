286fe144175d0ddbab9d5b361297d4bdef8ffa2b
Mostafa Hafezypoor <m.folad1383@gmail.com>
2026-09-10 09:05:05 +0330

    AddOrderMessenger.kt write image head back to MainActivity and back on pressed to MainActivity
    FViewPostChannel.kt add input view id channel , and onClick Listener btns Back

--- app/src/main/java/mostafa/hafezypoor/robiyar/ui/messenger/addOrder/AddOrderMessenger.kt
+++ app/src/main/java/mostafa/hafezypoor/robiyar/ui/messenger/addOrder/AddOrderMessenger.kt
@@ -1,5 +1,6 @@
 package mostafa.hafezypoor.robiyar.ui.messenger.addOrder
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.ImageView
@@ -7,11 +8,13 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import mostafa.hafezypoor.robiyar.MainActivity
 import mostafa.hafezypoor.robiyar.R
 import mostafa.hafezypoor.robiyar.utils.AnimationCard
 
 class AddOrderMessenger : AppCompatActivity() {
     private lateinit var imageHead : ImageView
+    private lateinit var imageHeadBack : ImageView
     private lateinit var textHead  : TextView
     private lateinit var card : FrameLayout
     private lateinit var actionBar: LinearLayout
@@ -19,6 +22,7 @@ class AddOrderMessenger : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_order_messenger)
         imageHead = findViewById<ImageView>(R.id.imageHead)
+        imageHeadBack = findViewById<ImageView>(R.id.imageHeadBack)
         textHead = findViewById<TextView>(R.id.textHead)
         card = findViewById<FrameLayout>(R.id.add_order_messenger_frame_layout)
         actionBar = findViewById<LinearLayout>(R.id.actionBar)
@@ -39,10 +43,15 @@ class AddOrderMessenger : AppCompatActivity() {
                 textHead.setText("نظر سنجی روبیکا")
             }
         }
+        imageHeadBack.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
 
     }
 
     override fun onBackPressed() {
-
+     startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
