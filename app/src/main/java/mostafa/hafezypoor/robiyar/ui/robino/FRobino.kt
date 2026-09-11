addde3f855bffaf93007d5be837f748cf8d8edbb
Mostafa Hafezypoor <m.folad1383@gmail.com>
2026-09-11 08:30:48 +0330

    write ui AddOrderRobino.kt , FRobino.kt onClickListener to addOrder activity

--- app/src/main/java/mostafa/hafezypoor/robiyar/ui/robino/FRobino.kt
+++ app/src/main/java/mostafa/hafezypoor/robiyar/ui/robino/FRobino.kt
@@ -1,10 +1,14 @@
 package mostafa.hafezypoor.robiyar.ui.robino
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.GridView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import mostafa.hafezypoor.robiyar.R
+import mostafa.hafezypoor.robiyar.ui.robino.addOrder.AddOrderRobino
+
 class FRobino : Fragment(R.layout.frobino) {
     private lateinit var list : GridView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -15,5 +19,14 @@ class FRobino : Fragment(R.layout.frobino) {
         model.add(ModelGridViewAdapterFRobino("لایک پست روبینو",R.drawable.post_reaction))
         model.add(ModelGridViewAdapterFRobino("ویو پست روبینو",R.drawable.cartoon_post_photo))
         list.adapter = GridViewAdapterFRobino(context,model)
+        list.setOnItemClickListener{parent,view,position,id ->
+            val intent = Intent(activity, AddOrderRobino::class.java)
+          when(position){
+              0 -> intent.putExtra("ItemClicked","FOLLOWER_ROBINO")
+              1 -> intent.putExtra("ItemClicked","LIKE_POST_ROBINO")
+              2 -> intent.putExtra("ItemClicked","VIEW_POST_ROBINO")
+          }
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/mostafa/hafezypoor/robiyar/ui/robino/addOrder/AddOrderRobino.kt
@@ -0,0 +1,67 @@
+package mostafa.hafezypoor.robiyar.ui.robino.addOrder
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import mostafa.hafezypoor.robiyar.MainActivity
+import mostafa.hafezypoor.robiyar.R
+import mostafa.hafezypoor.robiyar.ui.messenger.addOrder.FMemberChannel
+import mostafa.hafezypoor.robiyar.ui.messenger.addOrder.FMemberGroup
+import mostafa.hafezypoor.robiyar.ui.messenger.addOrder.FPoll
+import mostafa.hafezypoor.robiyar.ui.messenger.addOrder.FViewPostChannel
+import mostafa.hafezypoor.robiyar.utils.AnimationCard
+
+class AddOrderRobino : AppCompatActivity() {
+    private lateinit var imageHead : ImageView
+    private lateinit var imageHeadBack : ImageView
+    private lateinit var textHead  : TextView
+    private lateinit var card : FrameLayout
+    private lateinit var actionBar: LinearLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_order_robino)
+        imageHead = findViewById<ImageView>(R.id.imageHead)
+        imageHeadBack = findViewById<ImageView>(R.id.imageHeadBack)
+        textHead = findViewById<TextView>(R.id.textHead)
+        card = findViewById<FrameLayout>(R.id.add_order_messenger_frame_layout)
+        actionBar = findViewById<LinearLayout>(R.id.actionBar)
+        intent.getStringExtra("ItemClicked")?.let {
+            if (it == "VIEW_POST_CHANNEL"){
+            imageHead.setImageResource(R.drawable.solid_view)
+            textHead.setText("ویو پست کانال")
+                AnimationCard.expandAnimation(card,supportFragmentManager,R.id.add_order_messenger_frame_layout,
+                    FViewPostChannel(),"ViewPostChannel",actionBar,500)
+            }else if (it == "MEMBER_CHANNEL"){
+                imageHead.setImageResource(R.drawable.crowd_people)
+                textHead.setText("عضو کانال")
+                AnimationCard.expandAnimation(card,supportFragmentManager,R.id.add_order_messenger_frame_layout,
+                    FMemberChannel(),"FMemberChannel",actionBar,500)
+            }else if (it == "MEMBER_GROUP"){
+                imageHead.setImageResource(R.drawable.group_circle_discussing)
+                textHead.setText("عضو گروه")
+                AnimationCard.expandAnimation(card,supportFragmentManager,R.id.add_order_messenger_frame_layout,
+                    FMemberGroup(),"FMemberGroup",actionBar,500)
+            }else if (it == "POLL"){
+                imageHead.setImageResource(R.drawable.poll_follower_count)
+                textHead.setText("نظر سنجی روبیکا")
+                AnimationCard.expandAnimation(card,supportFragmentManager,R.id.add_order_messenger_frame_layout,
+                    FPoll(),"FPoll",actionBar,500)
+            }
+        }
+        imageHeadBack.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+    }
+
+    override fun onBackPressed() {
+     startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
